6e8087e22785d980d65b73aade5409114098a2c4
s3952320 <s3952320@student.rmit.edu.au>
2024-10-09 23:35:01 +1100

    Improved disclaimer page, and removed home page as it is redundant.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useRef, useState } from 'react';
 import DisclaimerPopup from './components/DisclaimerPopup'; //Imported DisclaimerPopup component 
-import HomePage from './components/HomePage'; // Imported HomePage component
 import { BottomPanelControl } from './components/BottomPanelControl';
 import { SidePanelControl } from './components/SidePanelControl';
 import { ArticleParticle } from './three.js/ArticleParticle';
@@ -15,18 +14,19 @@ function App() {
     const [isPlaying, setIsPlaying] = useState(false);
     const [isFullScreen, setIsFullScreen] = useState(false);
     const [isDisclaimerAccepted, setIsDisclaimerAccepted] = useState(false); // Controls disclaimer visibility
-    const [showHomePage, setShowHomePage] = useState(false); // Controls home page visibility
+    const [disclaimerOpacity, setDisclaimerOpacity] = useState(1);
     const audioRef = useRef<HTMLAudioElement | null>(null);
 
     const handleEnterSite = () => {
-        setShowHomePage(false); // Hide home page to show main content
+        setIsDisclaimerAccepted(false); // Hide home page to show main content
     };
 
     const handleDisclaimerAccept = () => {
-        setIsDisclaimerAccepted(true); // Hide disclaimer
-        setShowHomePage(true); // Show home page after disclaimer is accepted
-    };
-
+        setDisclaimerOpacity(0);
+        setTimeout(() => {
+          setIsDisclaimerAccepted(true);
+        }, 2000);
+      };
 
     // Bottom Panel Fullscreen
     const toggleFullScreen = () => {
@@ -86,12 +86,6 @@ function App() {
     if (!isDisclaimerAccepted) {
         return <DisclaimerPopup onAccept={handleDisclaimerAccept} />;
     }
-
-    // Render HomePage if the disclaimer has been accepted but the home page is not yet dismissed
-    if (showHomePage) {
-        return <HomePage onEnter={handleEnterSite} />;
-    }
-
 
     return (
         <div className='bg-black min-h-screen'>
