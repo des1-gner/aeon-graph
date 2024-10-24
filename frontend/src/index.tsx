2938c0abef3cbf4aa8e1ea8ad18f6ab1d1b23ff1
jasicajong00 <87701437+jasicajong00@users.noreply.github.com>
2024-10-24 12:59:06 +1100

    update logo and title

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ArticlesProvider } from './contexts/ArticlesContext';
+import logo from 'frontend/zone.jpg'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
